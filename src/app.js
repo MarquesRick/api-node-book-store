5b182d591127e9b2999182e37bbdfc74eed4690f
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-05-05 22:50:29 -0300

    create method put and getById

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const books = [
   {
     id: 1,
-    title: 'Lord Rings',
+    title: 'The Lord of the Rings',
   },
   {
     id: 2,
@@ -17,16 +17,31 @@ const books = [
 ];
 
 app.get('/', (req, res) => {
-  res.status(200).send('Test 1');
+  res.status(200).send('🙋🏻‍♂️👋 Welcome to API book store!');
 });
 
 app.get('/books', (req, res) => {
   res.status(200).json(books);
 });
 
+app.get('/books/:id', (req, res) => {
+  let index = getBook(req.params.id);
+  res.status(200).json(books[index]);
+});
+
 app.post('/books', (req, res) => {
   books.push(req.body);
   res.status(201).send('✅ Book registered successfully!');
 });
+
+app.put('/books/:id', (req, res) => {
+  let index = getBook(req.params.id);
+  books[index].title = req.body.title;
+  res.status(200).json(books);
+});
+
+const getBook = (id) => {
+  return books.findIndex((book) => book.id == id);
+};
 
 export default app;
