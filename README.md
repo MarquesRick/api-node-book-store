332ab6680cc7228442daeca83d0c4a918e43c1b3
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-05-05 21:35:11 -0300

    create routes, install nodemon and create .gitignore

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ```bash
 # create project 
 npm init -y
+
+# install nodemon
+npm install nodemon@2.0.15 -D
 ```
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = 3000;
 
+const routes = {
+  '/': 'Test 1',
+  '/books': 'Page books',
+  '/author': 'Page author',
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Node request test');
+  res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
